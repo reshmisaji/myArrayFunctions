e937d7e1fefdc24457cae97fbaa70da172c2e6f8
Reshmi R S <44020004+reshmisaji@users.noreply.github.com>
2018-11-20 19:26:32 +0530

    add test cases for filter with string

--- src/arraysLibrary.js
+++ src/arraysLibrary.js
@@ -3,4 +3,12 @@ const isEven = function(number){
   return number % 2;
 }
 
+const hasVowels = function(string){
+  let result = string.match(/["a","e","i","o","u"]/g)
+  let returnValue = result == null? 1 : 0;
+
+  return returnValue;
+}
+
 exports.isEven = isEven;
+exports.hasVowels = hasVowels;
--- test/arrays_tests.js
+++ test/arrays_tests.js
@@ -2,14 +2,23 @@ const assert = require('assert');
 const arraysLibrary = require('../src/arraysLibrary.js');
 const arrayFunctionsLibrary = require('../src/arrayFunctionsLibrary.js');
 const {filter} = arrayFunctionsLibrary;
-const {isEven,mapLength} = arraysLibrary;
+const {isEven,hasVowels} = arraysLibrary;
 
 describe('Filter',function(){
-  it('should filter the input array with the providing function',function(){
+  it('should filter the input array with numbers and the providing function',function(){
     assert.deepEqual(filter(isEven,[]),[]);
     assert.deepEqual(filter(isEven,[0]),[0]);
     assert.deepEqual(filter(isEven,[0,1]),[0]);
     assert.deepEqual(filter(isEven,[0,2]),[0,2]);
     assert.deepEqual(filter(isEven,[1,3]),[]);
+  });
+
+  it('should filter the input array with string and the providing function',function(){
+   assert.deepEqual(filter(hasVowels,[]),[]);  
+   assert.deepEqual(filter(hasVowels,["a"]),["a"]);  
+   assert.deepEqual(filter(hasVowels,["b"]),[]);  
+   assert.deepEqual(filter(hasVowels,["b","a"]),["a"]);  
+   assert.deepEqual(filter(hasVowels,["b","c"]),[]);  
+   assert.deepEqual(filter(hasVowels,["a","e"]),["a","e"]);  
   });
 });
