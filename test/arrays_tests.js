c368ed1cb8cfec0cffd9d6b93574638705649a7d
Reshmi R S <44020004+reshmisaji@users.noreply.github.com>
2018-11-20 17:59:46 +0530

    changle test frame into mocha test frame

---
+++ test/arrays_tests.js
@@ -0,0 +1,15 @@
+const assert = require('assert');
+const arraysLibrary = require('../src/arraysLibrary.js');
+const arrayFunctionsLibrary = require('../src/arrayFunctionsLibrary.js');
+const {filter} = arrayFunctionsLibrary;
+const {isEven,mapLength} = arraysLibrary;
+
+describe('Filter',function(){
+  it('should filter the input array with the providing function',function(){
+assert.deepEqual(filter(isEven,[]),[]);
+assert.deepEqual(filter(isEven,[0]),[0]);
+assert.deepEqual(filter(isEven,[0,1]),[0]);
+assert.deepEqual(filter(isEven,[0,2]),[0,2]);
+assert.deepEqual(filter(isEven,[1,3]),[]);
+  });
+});
